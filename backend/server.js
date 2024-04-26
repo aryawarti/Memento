2ab2f79978063c4beb69e876d9e83e561db9c906
aryawarti <113262981+aryawarti@users.noreply.github.com>
2024-04-26 15:06:44 +0530

    Initialized our Backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const notes=require('./data/notes');
+const express = require("express");
+const dotenv= require('dotenv');
+
+const app=express();
+dotenv.config();
+
+app.get("/",(req,res)=>{
+      res.send("API is running");
+    });
+
+app.get('/api/notes',(req,res)=>{
+    res.json(notes);
+});
+
+app.get('/api/notes/:id',(req,res)=>{
+    const note=notes.find((n)=>n._id==req.params.id);
+    res.send(note);
+});
+
+const PORT = process.env.PORT|| 4000;
+console.log(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <div>Ashish Kumar Yadav</div>
+    </>
+  )
+}
+
+export default App
